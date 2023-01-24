73d8e66a12df62d6ad2802d5b2d8ea81c1834298
harimwoo <harimwoo@gmail.com>
2023-01-24 17:33:10 -0500

    first commit

---
+++ src/components/Note.js
@@ -0,0 +1,14 @@
+const Note = ({note, toggleImportance}) => {
+  const label = note.important ? 
+    'mark unimportant' : 'mark important'
+  
+    return(
+      <li className='note'>
+        {note.content}
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
+
+    )
+  }
+
+  export default Note
